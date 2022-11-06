61e700db01c84785a8e91769fab00cf29ff56af0
Natalia <nataliarojaslon9@gmail.com>
2022-11-06 01:14:51 -0500

    login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -6,10 +6,81 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(        
-      body: Column(
-        children: <Widget>[
-          Image.asset("assets/images/logo.png"),
-        ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: <Widget>[
+              const SizedBox(
+                height: 50,
+              ),
+              Image.asset("assets/images/logoapp.png"),
+              const SizedBox(
+                height: 16,
+              ),
+              const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Correo electronico',
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Contraseña',
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              ElevatedButton(
+                onPressed: (){}, 
+                child: const Text("Iniciar sesión")
+                ),
+                const SizedBox(
+                height: 12,
+              ),
+              const Text("O iniciar sesión con:"),
+              const SizedBox(
+                height: 12,
+              ),
+              Row ( 
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                ElevatedButton(
+                  onPressed: (){}, 
+                  child: const Text("Facebook")
+                  ),
+                ElevatedButton(
+                  onPressed: (){}, 
+                  child: const Text("Google")
+                  ),
+              
+                ],
+              ),
+              const SizedBox(
+                height: 12,
+              ), 
+              const Text("Aún no tienes cuenta?"),
+              const SizedBox(
+                height: 12,
+              ),
+              TextButton(
+                onPressed: (){}, 
+                child: const Text("Registrate ahora")
+                ),
+              const SizedBox(
+                height: 12,
+              ),
+              
+            ],
+          ),
+        ),
       ),
     );
   }
