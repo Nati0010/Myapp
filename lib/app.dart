2b5c8489ade7c6bbfca46e3b95b7809749d6cba6
Natalia <nataliarojaslon9@gmail.com>
2022-11-12 20:56:03 -0500

    app.

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
-import '/view/pages/createaccount.dart';
-
+import '/view/pages/login.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title:"Wander Books",
-      home: CreateAccount (),
+      home: LoginPage(),
     );
   }
 }
